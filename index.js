4bbb583ba4e4e095784865c25d030981e4d762a1
Jonathan Chan <johnkchan252@gmail.com>
2019-06-13 03:40:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 function getFirstSelector(selector) {
  return document.querySelector(selector);
 }
+
+function nestedTarget() {
+  
+}
+
+function increaseRankBy(n) {
+  const lis = document.querySelectorAll('.ranked-list');
+  
+  
+}
