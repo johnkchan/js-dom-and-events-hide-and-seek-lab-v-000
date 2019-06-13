8dc48a4ef78375342e7d26ab7b4b6b010d55808b
Jonathan Chan <johnkchan252@gmail.com>
2019-06-13 03:56:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function increaseRankBy(n) {
 }
 
 function deepestChild() {
-  const grandNode = document.getElementById('grand-node').getElementsByTagName('div')[3];
+  const grandNode = document.getElementById('grand-node').getElementsByTagName('div')[3].innerHTML;
   
   return grandNode;
 }
