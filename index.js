d51aae5044b8f533e4bb78d8b50f508427b5cb81
Jonathan Chan <johnkchan252@gmail.com>
2019-06-13 03:43:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function getFirstSelector(selector) {
 }
 
 function nestedTarget() {
-  return document.querySelector('#nested.target');
+  return document.getElementById('nested')querySelector('#nested.target');
 }
 
 function increaseRankBy(n) {
